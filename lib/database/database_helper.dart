2d7c60f9b13985353e2e8b24175bc3d32879674f
ekinncem <ekinn.ciem16@gmail.com>
2025-01-25 03:50:38 +0300

    new

---
+++ lib/database/database_helper.dart
@@ -0,0 +1,67 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('subscriptions.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB,
+    );
+  }
+
+  Future _createDB(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE subscriptions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        price REAL,
+        date TEXT
+      )
+    ''');
+  }
+
+  Future<int> insertSubscription(Subscription subscription) async {
+    final db = await instance.database;
+    return await db.insert('subscriptions', subscription.toMap());
+  }
+
+  Future<List<Subscription>> getAllSubscriptions() async {
+    final db = await instance.database;
+    final maps = await db.query('subscriptions');
+    return maps.map((map) => Subscription.fromMap(map)).toList();
+  }
+
+  Future<int> updateSubscription(Subscription subscription) async {
+    final db = await instance.database;
+    return await db.update(
+      'subscriptions',
+      subscription.toMap(),
+      where: 'id = ?',
+      whereArgs: [subscription.id],
+    );
+  }
+
+  Future<int> deleteSubscription(int id) async {
+    final db = await instance.database;
+    return await db.delete(
+      'subscriptions',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+}
