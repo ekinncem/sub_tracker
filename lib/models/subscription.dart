902c7bef62c3488ed2a437e32568f4ebd370c487
ekinncem <ekinn.ciem16@gmail.com>
2025-01-25 03:50:12 +0300

    new

---
+++ lib/models/subscription.dart
@@ -0,0 +1,31 @@
+class Subscription {
+  int? id;
+  String name;
+  double price;
+  DateTime date;
+
+  Subscription({
+    this.id,
+    required this.name,
+    required this.price,
+    required this.date,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'price': price,
+      'date': date.toIso8601String(),
+    };
+  }
+
+  factory Subscription.fromMap(Map<String, dynamic> map) {
+    return Subscription(
+      id: map['id'],
+      name: map['name'],
+      price: map['price'],
+      date: DateTime.parse(map['date']),
+    );
+  }
+}
