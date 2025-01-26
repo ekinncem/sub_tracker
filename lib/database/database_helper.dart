4faf4e2001f56e9c395af61d40ab08801e6d3398
ekinncem <ekinn.ciem16@gmail.com>
2025-01-26 22:13:00 +0300

    new

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -53,7 +53,7 @@ class DatabaseHelper {
       'subscriptions',
       subscription.toMap(),
       where: 'id = ?',
-      whereArgs: [subscrasdiption.id],
+      whereArgs: [subscription.id],
     );
   }
 
